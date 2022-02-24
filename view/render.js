c5afb81fe97dcf4efc1d39a9106bdf4452875f77
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-02-24 16:00:02 +0300

    webpack mvc yarn

---
+++ view/render.js
@@ -0,0 +1,7 @@
+import { contentRehder } from "./content.js"
+
+export const render = () => {
+  const root = document.getElementById("root")
+  const tabl = contentRehder()
+  root.innerHTML = tabl
+}
